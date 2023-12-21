c3e23c677abef8214c816d45c374a833120799f4
AliMustafa253 <alimustafa2599@gmail.com>
2023-12-22 02:10:29 +0500

    updated agent setup

--- agent/agent_setup.py
+++ agent/agent_setup.py
@@ -1,9 +1,10 @@
-# !pip install llama-index google-generativeai openai pypdf pillow python-dotenv
+# !pip install llama-index google-generativeai openai pypdf pillow python-dotenv llama_hub youtube_transcript_api flask_cors
 
 import os
 import openai
 import logging
 import sys
+import json
 from llama_index.tools import BaseTool, FunctionTool
 from llama_index.multi_modal_llms.gemini import GeminiMultiModal
 from llama_index.multi_modal_llms.generic_utils import load_image_urls
@@ -15,9 +16,10 @@ from llama_index import (
   StorageContext,
   load_index_from_storage,
   )
+from llama_hub.youtube_transcript import YoutubeTranscriptReader
 from llama_index.tools import QueryEngineTool, ToolMetadata
 from llama_index.agent import OpenAIAgent
-from llama_index.llms import OpenAI
+from llama_index.llms import OpenAI,Gemini
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -132,7 +134,63 @@ def agent_setup():
 
     save_tool = FunctionTool.from_defaults(fn=save_session)
 
-    """### Mindfulness routine recomendation tool"""
+    """### Mindfulness routine recomendation tool
+
+    Use Gemini Pro to examine the transcript of our mindfulness videos
+    """
+
+    # Download video transcripts
+    loader = YoutubeTranscriptReader()
+    documents = loader.load_data(ytlinks=['https://youtu.be/nPvN1OI7h80?si=iB3TYXI-9Ztc0Qpi',
+                                        'https://youtu.be/8ZhjZD8rj3E?si=n30dJ0D55tRl9frP',
+                                        'https://youtu.be/mGoGYu7F2PA?si=qsvemuKLDdlnvC6z',
+                                        'https://youtu.be/d0VZk3Dd0nw?si=yb3Zr4XGErHSENFB',
+                                        'https://youtu.be/mme5NC0F7wQ?si=Dd_KECuQCjPJSFXu',
+                                        'https://youtu.be/iPjFd1eL40Y?si=3RI6N1mw6J3unntk'
+                                        'https://youtu.be/iPjFd1eL40Y?si=bC0hbYPR0jbVRim9',
+                                        'https://youtu.be/zVbRxs_QFBA?si=Bn7MC5yG2QcXtHZP',
+                                        'https://youtu.be/6xTx984jSFc?si=J3rNB2v9OUvAEBsx',
+                                        'https://youtu.be/1qBbuKWWTGY?si=GO7hyw9za62_V9cB',
+                                        'https://youtu.be/fj0dh_KxIg4?si=dxVvRm6AC3Ixls1b',
+                                        'https://youtu.be/4ksAYqRku-s?si=q3cJ7Spvbgi6H2gi',
+                                        'https://youtu.be/f2ZwrQF6VQM?si=ujQw2fq5Ww3_b9Mv',
+                                        'https://youtu.be/IL_1DRZDzWc?si=-RutJc161ZO5wgO4'])
+
+
+    videos = {}
+
+    # Have Gemini analyze the transcript sentiment and who this video is for
+    for document in documents:
+        response = Gemini(model='models/gemini-pro', api_key=os.environ.get('GOOGLE_API_KEY')).complete(f"Summarize the transcript from this mindfulness video and who should use it: {document.text}")
+        videos[document.metadata['video_id']] = response.text
+
+    """Create a tool which recomends a mindfulness routine based on how the user is feeling"""
+
+
+    json_string = json.dumps(videos)
+
+
+    def recomend_mindfulness(feelings_summary: str) -> str:
+        """Recomends a mindfullness routine based on how the user is feeling
+            Returns: A string with a description of the mindfulness routine it recomends and a link to the youtube video
+
+        Args:
+            feelings_summary (str): A summary of the user's feeings
+        """
+        videos_string = json.dumps(videos)
+
+        response = Gemini(model='models/gemini-pro', api_key=os.environ.get('GOOGLE_API_KEY')).complete(f"""
+            I'm sending you a json with a list of youtube mindfulness videos
+            The key is the youtube video id and the value is a summary of the video transcript
+            from this mindfulness video and who should use it.  The json is here: {videos_string}.
+            Recomend a video based on my feelings and include the youtube link in the format https://www.youtube.com/watch?v=video_id
+            Here is a summary of the user's feelings: {feelings_summary}""")
+
+        return response.text
+
+    recommend_tool = FunctionTool.from_defaults(fn=recomend_mindfulness)
+
+    """### Query engine tool"""
 
     query_engine_tools = [
         QueryEngineTool(
@@ -167,13 +225,14 @@ def agent_setup():
         ),
     ]
 
-    tools = query_engine_tools + [vision_tool, save_tool]
+    tools = query_engine_tools + [vision_tool, save_tool, recommend_tool]
 
-    SYSTEM_PROMPT = """You are an emotional support assistant with the expertise of an experienced counselor. Your primary role is to assist the user by encouraging them to provide a drawing that conveys their mood.
-    You offer professional, friendly, and helpful guidance based on current counseling and mindfulness practices. Once you receive the image, interpret it to discern what the user might be feeling and confirm with them if your observation is correct.
-    If your interpretation does not align with their feelings, engage in a dialogue until you accurately understand their mood. Your knowledge is exclusively focused on understanding the user's emotions and recommending mindfulness routines using the tool, tailored to their mood.
+    SYSTEM_PROMPT = """You are an emotional support assistant with the expertise of an experienced counselor. Your primary role is to assist the user by encouraging them to provide a drawing that conveys their mood and then recomending a mindfulness routine.
+    You offer professional, friendly, and helpful guidance based on current counseling and mindfulness practices. Once you receive the image, interpret it using your tools to discern what the user might be feeling and confirm with them if your observation is correct.
+    If your interpretation does not align with their feelings, engage in a dialogue until you accurately understand their mood. Your knowledge is exclusively focused on understanding the user's emotions and recommending mindfulness routines using your tools tools, tailored to their mood.
+    Your responses should be limited to one sentence when possible so the user doesn't have to do a lot of reading.  An example response is "For feels of {insert feelings here} I recomend this mindfulness routine {insert link here}.
     Thus, you will only provide responses related to these areas. If a question falls outside your area of expertise or if you lack the necessary information, you will inform the user by saying,
-    'Sorry, I do not know the answer to your question.' and then prompt for more information related to their feelings. Once they confirm that you have correctly understood their feelings, your task is to recommend a suitable mindfulness routine using the tool."""
+    'Sorry, I do not know the answer to your question.' and then prompt for more information related to their feelings."""
 
     llm = OpenAI(model="gpt-4-1106-preview") # Using GPT-4 Turbo (Beta)
 
@@ -190,6 +249,7 @@ def agent_setup():
     # response = agent.chat("https://ih1.redbubble.net/image.3636044620.1142/bg,f8f8f8-flat,750x,075,f-pad,750x1000,f8f8f8.u5.jpg")
     # print("**Response:**\n\n",response)
     # print(agent)
+    # gent.reset()
     return agent
 
 # print("Agent Setup",agent)
